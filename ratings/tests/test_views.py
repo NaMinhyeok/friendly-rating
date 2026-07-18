6ff802e96fc4e1b25a1f4e6ff59f38f1ad6e9a5a
NaMinhyeok <nmh9097@gmail.com>
2026-07-18 15:41:17 +0900

    fix: exempt Railway health check from HTTPS redirect

--- ratings/tests/test_views.py
+++ ratings/tests/test_views.py
@@ -1,4 +1,4 @@
-from django.test import SimpleTestCase
+from django.test import SimpleTestCase, override_settings
 from django.urls import reverse
 
 
@@ -8,3 +8,9 @@ class BasicViewTests(SimpleTestCase):
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.content, b"ok")
+
+    @override_settings(SECURE_SSL_REDIRECT=True)
+    def test_health_check_bypasses_https_redirect_for_railway(self):
+        response = self.client.get(reverse("health"))
+
+        self.assertEqual(response.status_code, 200)
